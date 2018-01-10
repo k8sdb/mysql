9acfb71a6618de37f13a5a08ad0cadb35d2d3dd5
the-redback <maruf@appscode.com>
2018-01-10 17:24:53 +0600

    Script Fixed

--- hack/docker/mysql/8.0/make.sh
+++ hack/docker/mysql/8.0/make.sh
@@ -4,11 +4,12 @@ set -xeou pipefail
 DOCKER_REGISTRY=${DOCKER_REGISTRY:-kubedb}
 IMG=mysql
 TAG=8.0
+ALT_TAG=8
 
 docker pull $IMG:$TAG
 
 docker tag $IMG:$TAG "$DOCKER_REGISTRY/$IMG:$TAG"
 docker push "$DOCKER_REGISTRY/$IMG:$TAG"
 
-docker tag $IMG:$TAG "$DOCKER_REGISTRY/$IMG:8"
-docker push "$DOCKER_REGISTRY/$IMG:8"
+docker tag $IMG:$TAG "$DOCKER_REGISTRY/$IMG:$ALT_TAG"
+docker push "$DOCKER_REGISTRY/$IMG:$ALT_TAG"
