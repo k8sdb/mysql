5ae5d948e42d2f16700c4eb8f76404fad70e2ff0
the-redback <maruf@appscode.com>
2018-02-16 12:55:32 +0600

    revendored

--- pkg/validator/validate.go
+++ pkg/validator/validate.go
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	mysqlVersions = sets.NewString("8.0")
+	mysqlVersions = sets.NewString("8.0", "8")
 )
 
 func ValidateMySQL(client kubernetes.Interface, mysql *api.MySQL) error {
