797838f2b5d80f2ad58c676203158247c75ef851
the-redback <maruf@appscode.com>
2018-06-27 14:47:35 +0600

    Improved script

--- hack/dev/update-docker.sh
+++ hack/dev/update-docker.sh
@@ -4,16 +4,65 @@ set -xeou pipefail
 GOPATH=$(go env GOPATH)
 REPO_ROOT=$GOPATH/src/github.com/kubedb/mysql
 
-# $REPO_ROOT/hack/docker/mysql/5.7/make.sh
-# $REPO_ROOT/hack/docker/mysql/8.0/make.sh
+export DB_UPDATE=1
+export TOOLS_UPDATE=1
+export OPERATOR_UPDATE=1
 
-$REPO_ROOT/hack/docker/mysql-tools/5.7/make.sh build
-$REPO_ROOT/hack/docker/mysql-tools/5.7/make.sh push
+show_help() {
+    echo "update-docker.sh [options]"
+    echo " "
+    echo "options:"
+    echo "-h, --help                       show brief help"
+    echo "    --db-only                    update only database images"
+    echo "    --tools-only                 update only database-tools images"
+    echo "    --operator-only              update only operator image"
+}
 
-$REPO_ROOT/hack/docker/mysql-tools/8.0/make.sh build
-$REPO_ROOT/hack/docker/mysql-tools/8.0/make.sh push
+while test $# -gt 0; do
+    case "$1" in
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+        --db-only)
+            export DB_UPDATE=1
+            export TOOLS_UPDATE=0
+            export OPERATOR_UPDATE=0
+            shift
+            ;;
+        --tools-only)
+            export DB_UPDATE=0
+            export TOOLS_UPDATE=1
+            export OPERATOR_UPDATE=0
+            shift
+            ;;
+        --operator-only)
+            export DB_UPDATE=0
+            export TOOLS_UPDATE=0
+            export OPERATOR_UPDATE=1
+            shift
+            ;;
+        *)
+            show_help
+            exit 1
+            ;;
+    esac
+done
 
+if [ "$DB_UPDATE" -eq 1 ]; then
+    $REPO_ROOT/hack/docker/mysql/5.7/make.sh
+    $REPO_ROOT/hack/docker/mysql/8.0/make.sh
+fi
 
-# $REPO_ROOT/hack/docker/my-operator/make.sh build
-# $REPO_ROOT/hack/docker/my-operator/make.sh push
+if [ "$TOOLS_UPDATE" -eq 1 ]; then
+    $REPO_ROOT/hack/docker/mysql-tools/5.7/make.sh build
+    $REPO_ROOT/hack/docker/mysql-tools/5.7/make.sh push
 
+    $REPO_ROOT/hack/docker/mysql-tools/8.0/make.sh build
+    $REPO_ROOT/hack/docker/mysql-tools/8.0/make.sh push
+fi
+
+if [ "$OPERATOR_UPDATE" -eq 1 ]; then
+    $REPO_ROOT/hack/docker/my-operator/make.sh build
+    $REPO_ROOT/hack/docker/my-operator/make.sh push
+fi
