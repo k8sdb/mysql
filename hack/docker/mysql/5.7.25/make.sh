dce39afe215d25a72264b2bc7baf961797e0a876
shudipta <shudipta@appscode.com>
2019-05-03 14:58:44 +0600

    Update peer-finder comment

--- hack/docker/mysql/5.7.25/make.sh
+++ hack/docker/mysql/5.7.25/make.sh
@@ -16,7 +16,7 @@ build() {
   pushd "$REPO_ROOT/hack/docker/mysql/$DB_VERSION"
 
   # Download Peer-finder
-  # ref: peer-finder: https://github.com/kubernetes/contrib/tree/master/peer-finder
+  # ref: peer-finder: https://github.com/kmodules/peer-finder/releases/download/v1.0.1-ac/peer-finder
   # wget peer-finder: https://github.com/kubernetes/charts/blob/master/stable/mongodb-replicaset/install/Dockerfile#L18
   wget -qO peer-finder https://github.com/kmodules/peer-finder/releases/download/v1.0.1-ac/peer-finder
   chmod +x peer-finder
