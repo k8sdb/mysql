aa33e58e5c891547a432e9b917ded6165e7992e8
Tamal Saha <tamal@appscode.com>
2020-09-17 14:35:38 -0700

    Add license verifier (#233)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -28,6 +28,7 @@ import (
 	myAdmsn "kubedb.dev/mysql/pkg/admission"
 	"kubedb.dev/mysql/pkg/controller"
 
+	license "go.bytebuilders.dev/license-verifier/kubernetes"
 	admission "k8s.io/api/admission/v1beta1"
 	core "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -140,6 +141,8 @@ func (c completedConfig) New() (*MySQLServer, error) {
 			},
 		)
 	}
+
+	license.NewLicenseEnforcer(c.OperatorConfig.ClientConfig, c.OperatorConfig.LicenseFile).Install(genericServer.Handler.NonGoRestfulMux)
 
 	ctrl, err := c.OperatorConfig.New()
 	if err != nil {
