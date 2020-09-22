5482fbbc3ed1d18dff99b57ca47d3b2a56c3c5d7
Emruz Hossain <emruz@appscode.com>
2020-09-22 15:54:03 +0600

    Remove unused StashClient
    Signed-off-by: Emruz Hossain <emruz@appscode.com>

--- pkg/controller/config.go
+++ pkg/controller/config.go
@@ -30,7 +30,6 @@ import (
 	reg_util "kmodules.xyz/client-go/admissionregistration/v1beta1"
 	"kmodules.xyz/client-go/discovery"
 	appcat_cs "kmodules.xyz/custom-resources/client/clientset/versioned"
-	scs "stash.appscode.dev/apimachinery/client/clientset/versioned"
 )
 
 const (
@@ -50,7 +49,6 @@ type OperatorConfig struct {
 	CRDClient        crd_cs.Interface
 	DBClient         cs.Interface
 	DynamicClient    dynamic.Interface
-	StashClient      scs.Interface
 	AppCatalogClient appcat_cs.Interface
 	PromClient       pcm.MonitoringV1Interface
 	Recorder         record.EventRecorder
