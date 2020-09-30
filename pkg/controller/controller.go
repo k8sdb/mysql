8b31917c9a9b99d574ba878ff6a0bc5800d8ee7e
Emruz Hossain <emruz@appscode.com>
2020-09-30 16:45:38 +0600

    Add comment
    Signed-off-by: Emruz Hossain <emruz@appscode.com>

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -158,6 +158,7 @@ func (c *Controller) StartAndRunControllers(stopCh <-chan struct{}) {
 	// Initialize and start Stash controllers
 	go stash.NewController(c.Controller, &c.Config.Initializers.Stash, c.WatchNamespace).StartAfterStashInstalled(c.MaxNumRequeues, c.NumThreads, c.selector, stopCh)
 
+	// Start MySQL controller
 	c.RunControllers(stopCh)
 
 	<-stopCh
