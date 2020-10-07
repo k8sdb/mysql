6d1c0fa8da2c565413ec0fcc96b8b35b2cfcd99f
Tamal Saha <tamal@appscode.com>
2020-10-07 04:02:21 -0700

    Cleanup monitoring spec api (#250)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/controller/service.go
+++ pkg/controller/service.go
@@ -95,8 +95,8 @@ func (c *Controller) ensurePrimaryService(mysql *api.MySQL) (kutil.VerbType, err
 
 func (c *Controller) ensureStatsService(mysql *api.MySQL) (kutil.VerbType, error) {
 	// return if monitoring is not prometheus
-	if mysql.GetMonitoringVendor() != mona.VendorPrometheus {
-		log.Infoln("spec.monitor.agent is not operator or builtin.")
+	if mysql.Spec.Monitor == nil || mysql.Spec.Monitor.Agent.Vendor() != mona.VendorPrometheus {
+		log.Infoln("spec.monitor.agent is not provided by prometheus.io")
 		return kutil.VerbUnchanged, nil
 	}
 
@@ -113,10 +113,10 @@ func (c *Controller) ensureStatsService(mysql *api.MySQL) (kutil.VerbType, error
 		in.Spec.Selector = mysql.OffshootSelectors()
 		in.Spec.Ports = core_util.MergeServicePorts(in.Spec.Ports, []core.ServicePort{
 			{
-				Name:       api.PrometheusExporterPortName,
+				Name:       mona.PrometheusExporterPortName,
 				Protocol:   core.ProtocolTCP,
 				Port:       mysql.Spec.Monitor.Prometheus.Exporter.Port,
-				TargetPort: intstr.FromString(api.PrometheusExporterPortName),
+				TargetPort: intstr.FromString(mona.PrometheusExporterPortName),
 			},
 		})
 		return in
