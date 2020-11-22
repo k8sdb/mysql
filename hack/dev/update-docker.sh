cc80a56f45fa1b9c6b9105ae94ace549fa85bd09
Tamal Saha <tamal@appscode.com>
2020-11-22 09:06:09 -0800

    Format shell scripts (#312)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/dev/update-docker.sh
+++ hack/dev/update-docker.sh
@@ -14,9 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-
-
 set -eou pipefail
 
 GOPATH=$(go env GOPATH)
@@ -28,56 +25,56 @@ export EXPORTER_UPDATE=1
 export OPERATOR_UPDATE=1
 
 show_help() {
-  echo "update-docker.sh [options]"
-  echo " "
-  echo "options:"
-  echo "-h, --help                       show brief help"
-  echo "    --db-only                    update only database images"
-  echo "    --tools-only                 update only database-tools images"
-  echo "    --exporter-only              update only database-exporter images"
+    echo "update-docker.sh [options]"
+    echo " "
+    echo "options:"
+    echo "-h, --help                       show brief help"
+    echo "    --db-only                    update only database images"
+    echo "    --tools-only                 update only database-tools images"
+    echo "    --exporter-only              update only database-exporter images"
 }
 
 while test $# -gt 0; do
-  case "$1" in
-    -h | --help)
-      show_help
-      exit 0
-      ;;
-    --db-only)
-      export DB_UPDATE=1
-      export TOOLS_UPDATE=0
-      export EXPORTER_UPDATE=0
-      shift
-      ;;
-    --tools-only)
-      export DB_UPDATE=0
-      export TOOLS_UPDATE=1
-      export EXPORTER_UPDATE=0
-      shift
-      ;;
-    --exporter-only)
-      export DB_UPDATE=0
-      export TOOLS_UPDATE=0
-      export EXPORTER_UPDATE=1
-      shift
-      ;;
-    *)
-      show_help
-      exit 1
-      ;;
-  esac
+    case "$1" in
+        -h | --help)
+            show_help
+            exit 0
+            ;;
+        --db-only)
+            export DB_UPDATE=1
+            export TOOLS_UPDATE=0
+            export EXPORTER_UPDATE=0
+            shift
+            ;;
+        --tools-only)
+            export DB_UPDATE=0
+            export TOOLS_UPDATE=1
+            export EXPORTER_UPDATE=0
+            shift
+            ;;
+        --exporter-only)
+            export DB_UPDATE=0
+            export TOOLS_UPDATE=0
+            export EXPORTER_UPDATE=1
+            shift
+            ;;
+        *)
+            show_help
+            exit 1
+            ;;
+    esac
 done
 
 dbversions=(
-  5.7.25
-  5.7
-  8.0.3
-  8.0.14
-  8.0
+    5.7.25
+    5.7
+    8.0.3
+    8.0.14
+    8.0
 )
 
 exporters=(
-  v0.11.0
+    v0.11.0
 )
 
 echo ""
@@ -85,24 +82,24 @@ env | sort | grep -e DOCKER_REGISTRY -e APPSCODE_ENV || true
 echo ""
 
 if [ "$DB_UPDATE" -eq 1 ]; then
-  cowsay -f tux "Processing database images" || true
-  for db in "${dbversions[@]}"; do
-    ${REPO_ROOT}/hack/docker/mysql/${db}/make.sh build
-    ${REPO_ROOT}/hack/docker/mysql/${db}/make.sh push
-  done
+    cowsay -f tux "Processing database images" || true
+    for db in "${dbversions[@]}"; do
+        ${REPO_ROOT}/hack/docker/mysql/${db}/make.sh build
+        ${REPO_ROOT}/hack/docker/mysql/${db}/make.sh push
+    done
 fi
 
 if [ "$TOOLS_UPDATE" -eq 1 ]; then
-  cowsay -f tux "Processing database-tools images" || true
-  for db in "${dbversions[@]}"; do
-    ${REPO_ROOT}/hack/docker/mysql-tools/${db}/make.sh build
-    ${REPO_ROOT}/hack/docker/mysql-tools/${db}/make.sh push
-  done
+    cowsay -f tux "Processing database-tools images" || true
+    for db in "${dbversions[@]}"; do
+        ${REPO_ROOT}/hack/docker/mysql-tools/${db}/make.sh build
+        ${REPO_ROOT}/hack/docker/mysql-tools/${db}/make.sh push
+    done
 fi
 
 if [ "$EXPORTER_UPDATE" -eq 1 ]; then
-  cowsay -f tux "Processing database-exporter images" || true
-  for exporter in "${exporters[@]}"; do
-    ${REPO_ROOT}/hack/docker/mysqld-exporter/${exporter}/make.sh
-  done
+    cowsay -f tux "Processing database-exporter images" || true
+    for exporter in "${exporters[@]}"; do
+        ${REPO_ROOT}/hack/docker/mysqld-exporter/${exporter}/make.sh
+    done
 fi
--- hack/docker/mysql-tools/8.0.3/mysql-tools.sh
+++ hack/docker/mysql-tools/8.0.3/mysql-tools.sh
@@ -14,9 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-
-
 set -eou pipefail
 
 # ref: https://stackoverflow.com/a/7069755/244009
@@ -24,19 +21,19 @@ set -eou pipefail
 # ref: http://tldp.org/LDP/abs/html/comparison-ops.html
 
 show_help() {
-  echo "mysql-tools.sh - run tools"
-  echo " "
-  echo "mysql-tools.sh COMMAND [options]"
-  echo " "
-  echo "options:"
-  echo "-h, --help                         show brief help"
-  echo "    --data-dir=DIR                 path to directory holding db data (default: /var/data)"
-  echo "    --host=HOST                    database host"
-  echo "    --user=USERNAME                database username"
-  echo "    --bucket=BUCKET                name of bucket"
-  echo "    --folder=FOLDER                name of folder in bucket"
-  echo "    --snapshot=SNAPSHOT            name of snapshot"
-  echo "    --enable-analytics=ENABLE_ANALYTICS   send analytical events to Google Analytics (default true)"
+    echo "mysql-tools.sh - run tools"
+    echo " "
+    echo "mysql-tools.sh COMMAND [options]"
+    echo " "
+    echo "options:"
+    echo "-h, --help                         show brief help"
+    echo "    --data-dir=DIR                 path to directory holding db data (default: /var/data)"
+    echo "    --host=HOST                    database host"
+    echo "    --user=USERNAME                database username"
+    echo "    --bucket=BUCKET                name of bucket"
+    echo "    --folder=FOLDER                name of folder in bucket"
+    echo "    --snapshot=SNAPSHOT            name of snapshot"
+    echo "    --enable-analytics=ENABLE_ANALYTICS   send analytical events to Google Analytics (default true)"
 }
 
 RETVAL=0
@@ -56,60 +53,60 @@ op=$1
 shift
 
 while test $# -gt 0; do
-  case "$1" in
-    -h | --help)
-      show_help
-      exit 0
-      ;;
-    --data-dir*)
-      export DB_DATA_DIR=$(echo $1 | sed -e 's/^[^=]*=//g')
-      shift
-      ;;
-    --host*)
-      export DB_HOST=$(echo $1 | sed -e 's/^[^=]*=//g')
-      shift
-      ;;
-    --user*)
-      export DB_USER=$(echo $1 | sed -e 's/^[^=]*=//g')
-      shift
-      ;;
-    --bucket*)
-      export DB_BUCKET=$(echo $1 | sed -e 's/^[^=]*=//g')
-      shift
-      ;;
-    --folder*)
-      export DB_FOLDER=$(echo $1 | sed -e 's/^[^=]*=//g')
-      shift
-      ;;
-    --snapshot*)
-      export DB_SNAPSHOT=$(echo $1 | sed -e 's/^[^=]*=//g')
-      shift
-      ;;
-    --analytics* | --enable-analytics*)
-      export ENABLE_ANALYTICS=$(echo $1 | sed -e 's/^[^=]*=//g')
-      shift
-      ;;
-    --)
-      shift
-      break
-      ;;
-    *)
-      show_help
-      exit 1
-      ;;
-  esac
+    case "$1" in
+        -h | --help)
+            show_help
+            exit 0
+            ;;
+        --data-dir*)
+            export DB_DATA_DIR=$(echo $1 | sed -e 's/^[^=]*=//g')
+            shift
+            ;;
+        --host*)
+            export DB_HOST=$(echo $1 | sed -e 's/^[^=]*=//g')
+            shift
+            ;;
+        --user*)
+            export DB_USER=$(echo $1 | sed -e 's/^[^=]*=//g')
+            shift
+            ;;
+        --bucket*)
+            export DB_BUCKET=$(echo $1 | sed -e 's/^[^=]*=//g')
+            shift
+            ;;
+        --folder*)
+            export DB_FOLDER=$(echo $1 | sed -e 's/^[^=]*=//g')
+            shift
+            ;;
+        --snapshot*)
+            export DB_SNAPSHOT=$(echo $1 | sed -e 's/^[^=]*=//g')
+            shift
+            ;;
+        --analytics* | --enable-analytics*)
+            export ENABLE_ANALYTICS=$(echo $1 | sed -e 's/^[^=]*=//g')
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            show_help
+            exit 1
+            ;;
+    esac
 done
 
 if [ -n "$DEBUG" ]; then
-  env | sort | grep DB_*
-  echo ""
+    env | sort | grep DB_*
+    echo ""
 fi
 
 # Wait for mysql to start
 # ref: http://unix.stackexchange.com/a/5279
 while ! nc -q 1 $DB_HOST $DB_PORT </dev/null; do
-  echo "Waiting... database is not ready yet"
-  sleep 5
+    echo "Waiting... database is not ready yet"
+    sleep 5
 done
 
 # cleanup data dump dir
@@ -118,28 +115,28 @@ cd "$DB_DATA_DIR"
 rm -rf *
 
 case "$op" in
-  backup)
-    echo "Dumping database......"
-    mysqldump -u ${DB_USER} --password=${DB_PASSWORD} -h ${DB_HOST} "$@" >dumpfile.sql
+    backup)
+        echo "Dumping database......"
+        mysqldump -u ${DB_USER} --password=${DB_PASSWORD} -h ${DB_HOST} "$@" >dumpfile.sql
 
-    echo "Uploading dump file to the backend......."
-    osm push --enable-analytics="$ENABLE_ANALYTICS" --osmconfig="$OSM_CONFIG_FILE" -c "$DB_BUCKET" "$DB_DATA_DIR" "$DB_FOLDER/$DB_SNAPSHOT"
+        echo "Uploading dump file to the backend......."
+        osm push --enable-analytics="$ENABLE_ANALYTICS" --osmconfig="$OSM_CONFIG_FILE" -c "$DB_BUCKET" "$DB_DATA_DIR" "$DB_FOLDER/$DB_SNAPSHOT"
 
-    echo "Backup successful"
-    ;;
-  restore)
-    echo "Pulling backup file from the backend"
-    osm pull --enable-analytics="$ENABLE_ANALYTICS" --osmconfig="$OSM_CONFIG_FILE" -c "$DB_BUCKET" "$DB_FOLDER/$DB_SNAPSHOT" "$DB_DATA_DIR"
+        echo "Backup successful"
+        ;;
+    restore)
+        echo "Pulling backup file from the backend"
+        osm pull --enable-analytics="$ENABLE_ANALYTICS" --osmconfig="$OSM_CONFIG_FILE" -c "$DB_BUCKET" "$DB_FOLDER/$DB_SNAPSHOT" "$DB_DATA_DIR"
 
-    echo "Inserting data into database........"
-    mysql -u "$DB_USER" --password=${DB_PASSWORD} -h "$DB_HOST" "$@" -f <dumpfile.sql
+        echo "Inserting data into database........"
+        mysql -u "$DB_USER" --password=${DB_PASSWORD} -h "$DB_HOST" "$@" -f <dumpfile.sql
 
-    echo "Recovery successful"
-    ;;
-  *)
-    (10)
-    echo $"Unknown op!"
-    RETVAL=1
-    ;;
+        echo "Recovery successful"
+        ;;
+    *)
+        (10)
+        echo $"Unknown op!"
+        RETVAL=1
+        ;;
 esac
 exit "$RETVAL"
